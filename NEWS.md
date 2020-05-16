c86bcadc6546434844ca61f138616a3bcd8d0425
Kevin Cazelles <kcazelle@uoguelph.ca>
2020-05-15 22:27:28 -0400

    :bookmark: v0.1.0

--- NEWS.md
+++ NEWS.md
@@ -1,8 +1,13 @@
+# biogeonet 0.1.0
+
+* Added functions to compute species richness and presence of individual species for all states (community).
+* All code is now avaible to reproduce results in Cazelles (2016).
+
 # biogeonet 0.0.2
 
 * Added a `NEWS.md` file to track changes to the compendium.
 * Use GitHub Actions.
-* Walkthrough to reproduce the analysis as a vignette. 
+* Walkthrough to reproduce the analysis as a vignette.
 * Add Supplementary Information as a vignette.
 * Explain the overall approach.
 
--- tests/testthat/test-buildMarkov.R
+++ tests/testthat/test-buildMarkov.R
@@ -8,9 +8,16 @@ colo <- runif(nbsp)
 mark <- buildMarkov(nbsp, colo, web, .2, 0.01, 0.5, 2)
 
 test_that("checking simple values", {
-  expect_equal(all(abs(colSums(mark))<10^-12), TRUE)
+  expect_equal(all(abs(colSums(mark$markov))<10^-12), TRUE)
 })
 
 test_that("checking errors", {
   expect_error(buildMarkov(nbsp, colo, web, .2, 0.5, 0.1, 2))
+})
+
+sprich <- mark$speciesRichness
+sppres <- mark$speciesPresence
+test_that("species richness and presence", {
+  expect_equal(sprich[c(1, length(sprich))], c(0, 5))
+  expect_true(all(apply(sppres, 1, sum) == 2^(nbsp-1)))
 })
